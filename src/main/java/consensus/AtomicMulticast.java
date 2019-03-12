fe5d900e177a3ce9639b201844d54d6283dc5b82
Ferdinand <ferdinandtt@gmail.com>
2019-03-12 15:53:14 +0100

    Cleaned up the code abit, removed comments and renamed an ansible file to reflect what it is actually doing

--- src/main/java/consensus/AtomicMulticast.java
+++ src/main/java/consensus/AtomicMulticast.java
@@ -6,6 +6,7 @@ import model.KafkaMessage;
 import model.Type;
 import org.apache.commons.lang3.SerializationUtils;
 import comparator.TimestampAscending;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
@@ -24,7 +25,8 @@ public class AtomicMulticast implements Atomic {
 
     public static AtomicMulticast instance;
 
-    private static final PriorityQueue<KafkaMessage> deliveryHeap = new PriorityQueue<>(new TimestampAscending());
+    // Heap based queue
+    private final PriorityQueue<KafkaMessage> deliveryHeap = new PriorityQueue<>(new TimestampAscending());
 
     protected Long logicalClock = 0L;
 
@@ -43,13 +45,6 @@ public class AtomicMulticast implements Atomic {
 
     }
 
-    /*
-    If a message is designated to a topic that does not exist, what to do then, we would never get a Ack message
-    from that topic. Do we timeout after a while and remove it?
-        - Need to notify user somehow? Process this as a message in deliver queue as failed message?
-        - What if messageID is not unique ?
-     */
-
     /**
      * Phase One is the content based multicast phase.
      * Ensuring all intended Topic receive the Message.
@@ -66,25 +61,23 @@ public class AtomicMulticast implements Atomic {
         // Both messages means the same, but comes from different origins.
         // And if a Node has not received a clientmessage, it will receive a notify message and treat it the same way.
 
-        // Dont care about numbers of messages here, just build the array and store messages.
-
 
         // If only one topic, skip phases and storing message and send it directly to kafka.
         if (msg.getTopic().length == 1) {
-            //System.out.println("Unique msg " + msg.toString());
             // Send message directly to phase 4.
             msg.setMessageType(Type.Delivery);
             msg.setTimeStamp(logicalClock);
             logicalClock++;
-            deliveryHeap.add(msg); // Delivery happens through deliveryHeap!
-            return null;
+            deliveryHeap.add(msg);
+            return null; // Delivery happens through deliveryHeap!
         }
 
-        // More than once receiver!
+        // More than one destination!
         // Update the timestamp!
         msg.setTimeStamp(logicalClock);
         logicalClock++;
 
+        // Create a state map
         ConcurrentHashMap<Integer, KafkaMessage> list = state.get(msg.getMessageID());
         if (list == null) {
             list = new ConcurrentHashMap<>();
@@ -102,12 +95,11 @@ public class AtomicMulticast implements Atomic {
         cloned.setSenderID(ConsumerThread.CLIENT_ID); // Set ourselves as sender.
 
 
-        // TODO: does each node have to "notify" all other nodes if this is already done ?
         if (msg.getMessageType() == Type.NotifyMessage) {
             // Respond to other nodes that we have gotten the message from client.
             cloned.setMessageType(Type.AckMessage);
         } else {
-            // Notify others that there are a new message TODO: Redo logic to be reactive instead of proactive.
+            // Notify others that there are a new message.
             cloned.setMessageType(Type.NotifyMessage);
         }
 
@@ -134,28 +126,25 @@ public class AtomicMulticast implements Atomic {
             this.deliveryHeap.add(msg);
         }
 
-        KafkaMessage storedMessage = null;
-
         if (msg.getMessageType() == Type.AckMessage) {
-            messageMap.put(msg.getSenderID(), msg);
-            storedMessage = msg;
 
-            // TODO: Update deliveryheap here.
+            messageMap.put(msg.getSenderID(), msg);
             if(!this.deliveryHeap.contains(msg)) {
                 this.deliveryHeap.add(msg);
             } else {
                 this.deliveryHeap.remove(msg);
                 this.deliveryHeap.add(msg);
-            }
+            } // TODO: Not sure if this is actually needed.
         }
 
         // Only check if we reponses from ALL topics, dont waste resources.
-        if(this.state.get(msg.getMessageID()).entrySet().size() == msg.getTopic().length) {
+        Set<Map.Entry<Integer, KafkaMessage>> messages = this.state.get(msg.getMessageID()).entrySet();
+        if(messages.size() == msg.getTopic().length) {
             System.out.println("All messages received, find if all is ack");
             boolean allAcks = true;
             PriorityQueue<KafkaMessage> queue = new PriorityQueue<>( new TimestampDecending());
 
-            for (Map.Entry<Integer, KafkaMessage> entry : this.state.get(msg.getMessageID()).entrySet()) {
+            for (Map.Entry<Integer, KafkaMessage> entry : messages) {
                 // A entry can be updated to decided if our queue is slow.
                 if (entry.getValue().getMessageType() != Type.AckMessage) {
                     allAcks = false;
@@ -182,51 +171,7 @@ public class AtomicMulticast implements Atomic {
 
     @Override
     public KafkaMessage phaseThree(KafkaMessage msg) {
-        // Check if we have achieved phase3, meaning every node has recieved the TS and offset.
-        // Send a decision message to every node.
-        /*if (this.state == null) {
-            System.out.println("State is null");
-        }
-
-        if (msg == null) {
-            System.out.println("MSG is null");
-        }
-
-        ConcurrentHashMap<Integer, KafkaMessage> messageMap = this.state.get(msg.getMessageID());
-        if (msg.getMessageType() == Type.Decided) {
-            messageMap.put(msg.getSenderID(), msg);
-        }
-
-        // No need to check every time if we have not gotten all responses yet!
-        Set entries = this.state.get(msg.getMessageID()).entrySet();
-        if(entries.size() == msg.getTopic().length) {
-
-            // TODO: Go through all deliverable messages and deliver by offsets. from earliest to latest.
-            boolean allDecided = true;
-            long matchOffset = msg.getOffset();
-            long matchTimeStamp = msg.getTimeStamp();
-            Set<Integer> haveTopicResponses = new HashSet<>();
-
-            for (Map.Entry<Integer, KafkaMessage> entry : this.state.get(msg.getMessageID()).entrySet()) {
-                // TODO: This should be done by the heap, only need to check which Timestamp is the lowest!
-                if (entry.getValue().getMessageType() != Type.Decided
-                        && entry.getValue().getTimeStamp() != matchTimeStamp
-                        && entry.getValue().getOffset() != matchOffset) {
-                    allDecided = false;
-                } else {
-                    // Store unique senders.
-                    haveTopicResponses.add(entry.getValue().getSenderID());
-                }
-            }
-            // TODO: REMOVE THIS, delivery will be done by heap
-            if (allDecided && haveTopicResponses.size() == msg.getTopic().length) {
-                KafkaMessage cloned = cloneMessage(msg);
-                cloned.setSenderID(ConsumerThread.CLIENT_ID);
-                cloned.setMessageType(Type.Delivery);
-                return cloned;
-            }
-        }*/
-        return null;
+        throw new  NotImplementedException();
     }
 
     @Override
@@ -244,7 +189,6 @@ public class AtomicMulticast implements Atomic {
         try {
             cloned = (KafkaMessage) msg.clone();
         } catch (CloneNotSupportedException e ) {
-            System.out.println("COULD NOT CLONE MSG codeline 267");
             cloned = SerializationUtils.clone(msg); // slow
         }
         return cloned;
@@ -254,15 +198,13 @@ public class AtomicMulticast implements Atomic {
         this.state.remove(msg.getMessageID());
     }
 
-
     public List<KafkaMessage> checkDelivery() {
+
         List<KafkaMessage> list = new ArrayList<>();
         if (this.deliveryHeap.peek() != null) {
-
             while (this.deliveryHeap.peek() != null &&
                     this.deliveryHeap.peek().getMessageType() == Type.Delivery)
             {
-                System.out.println(this.deliveryHeap.peek().toString() + " can be delivered!");
                 KafkaMessage msg = this.deliveryHeap.poll();
                 this.state.remove(msg.getMessageID());
                 list.add(msg);
@@ -270,4 +212,4 @@ public class AtomicMulticast implements Atomic {
         }
         return list;
     }
-}//{"messageID":1,"clientID":1,"messageType":"ClientMessage","value":"Hello","topic":["T1","T2"]}
+}
